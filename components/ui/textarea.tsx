bd5a6099f8c6545b84d65b292417b9daedfc2064
yusuken1121 <yusuken1121@gmail.com>
2024-10-01 18:13:36 -0700

    🐛 Ignore the error

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -3,8 +3,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {} // eslint-disable-line @typescript-eslint/no-empty-object-type
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
