8a5c724446d544eb08b9b23dc02238f92fd2cc50
yusuken1121 <yusuken1121@gmail.com>
2024-10-03 13:51:13 -0700

    🍱 Add a pic

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
-            Yusuke<span className="text-accent">.</span>
+            Yusuke<span className="text-accent text-shadow">.</span>
           </h1>
         </Link>
 
