5e3a1544562850b2dda0dc5dfe1a9259346c567f
yusuken1121 <yusuken1121@gmail.com>
2024-10-03 08:59:09 -0700

    ✅ Change the icon and links

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -5,11 +5,11 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 const socials = [
   {
     icon: <FaGithub />,
-    path: "https://github.com",
+    path: "https://github.com/yusuken1121",
   },
   {
     icon: <FaLinkedin />,
-    path: "https://github.com",
+    path: "https://www.linkedin.com/in/yusuke-nagaoka",
   },
 ];
 
