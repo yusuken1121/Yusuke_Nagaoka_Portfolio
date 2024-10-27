7ea4b07cfac6e476270ec41a54f5ef5740c24dc3
yusuken1121 <yusuken1121@gmail.com>
2024-10-27 16:39:14 -0700

    言語のlocaleを特定し、navアイテムをアクティブにする

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,19 +4,23 @@ import { usePathname } from "next/navigation";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { CiMenuFries } from "react-icons/ci";
 import Link from "next/link";
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 
 const MobileNav = () => {
   const pathname = usePathname();
   const t = useTranslations("Header");
 
+  const locale = useLocale();
+  const path = locale === "ja" ? "ja" : locale === "en" ? "en" : "/";
   const links = [
-    { name: t("home"), path: "/" },
-    { name: t("services"), path: "/services" },
-    { name: t("resume"), path: "/resume" },
-    { name: t("work"), path: "/work" },
-    { name: t("contact"), path: "/contact" },
+    { name: t("home"), path: `/${path}` },
+    { name: t("services"), path: `/${path}/services` },
+    { name: t("resume"), path: `/${path}/resume` },
+    { name: t("work"), path: `/${path}/work` },
+    { name: t("contact"), path: `/${path}/contact` },
   ];
+
+  console.log("⭐️", pathname, links[0].path);
   return (
     <Sheet>
       <SheetTrigger className="flex justify-center items-center">
